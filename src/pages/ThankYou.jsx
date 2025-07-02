fda540d67ab987fce9a26803b4792c94d4c13fd2
Pranav <mpranavprem@gmail.com>
2025-07-02 16:13:10 +0530

    changes

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -15,7 +15,7 @@ const ThankYou = () => {
   return (
     <>
       <div className="h-[calc(100vh-18rem)] flex flex-col items-center justify-center bg-tertiary">
-        <div className="py-4 sticky top-0 w-full bg-header/60 backdrop-blur-md z-50 text-white">
+        <div className="py-4 sticky top-0 w-full bg-header/60 backdrop-blur-md z-50 text-black">
           <div className="wrapper flex justify-between items-center gap-10">
             <div className="flex justify-between items-center gap-20 w-full pl-[1rem] lg:pl-0">
               <Link smooth={true} to="/" className="cursor-pointer">
@@ -32,14 +32,14 @@ const ThankYou = () => {
           </div>
         </div>
         <div className="wrapper flex flex-col items-center justify-center h-full ">
-          <h1 className="heading-1 text-secondary">Thank You</h1>
-          <p className="text-center text-secondary text-sm md:text-base text-balance">
+          <h1 className="heading-1 text-black">Thank You</h1>
+          <p className="text-center text-black text-sm md:text-base text-balance">
             Thanks for contacting us, one of our associates will get in touch
             with you soon!
           </p>
           <Link
             to={"/"}
-            className="mt-7 text-sm cursor-pointer primary-btn w-fit"
+            className="mt-7 text-sm cursor-pointer secondary-btn w-fit"
           >
             Home
           </Link>
